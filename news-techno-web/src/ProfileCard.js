49e3dfe84e2ba03cad7d0cf0886fdaf619617263
afifyahya210 <44243364+afifyahya210@users.noreply.github.com>
2021-11-25 23:14:40 +0700

    Update ProfileCard.js

--- news-techno-web/src/ProfileCard.js
+++ news-techno-web/src/ProfileCard.js
@@ -13,10 +13,10 @@ export default function ProfileCard (props) {
                 <p class="uraian"> {props.desc} </p>
                 
                 <div class="classButton">
-                    <button class="btng"> <FontAwesomeIcon icon={faGithub} /> </button> 
-                    <button class="btni"> <FontAwesomeIcon icon={faInstagram} /> </button> 
-                    <button class="btnw"> <FontAwesomeIcon icon={faWhatsapp} /> </button> 
-                    <button class="btnl"> <FontAwesomeIcon icon={faLinkedin} /> </button> 
+                    <a href="https://github.com/afifyahya210" target="_blank"> <button class="btng"> <FontAwesomeIcon icon={faGithub} /> </button></a>
+                    <a href="https://www.instagram.com/afifyahya210/" target="_blank"> <button class="btni"> <FontAwesomeIcon icon={faInstagram} /> </button></a>
+                    <a href="https://wa.me/6283148784298" target="_blank"> <button class="btnw"> <FontAwesomeIcon icon={faWhatsapp} /> </button> </a>
+                    <a href="https://www.linkedin.com/in/afif-yahya-102284184/" target="_blank"> <button class="btnl"> <FontAwesomeIcon icon={faLinkedin} /> </button> </a> 
                 </div>
                 <p class="nim"> {props.nim} </p>
             </div>
